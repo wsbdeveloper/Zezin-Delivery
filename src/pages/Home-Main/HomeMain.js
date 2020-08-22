08660399349f90f9434b079b6256c1df876aeacc
Wellington Bezerra <wellingtons.bezerra@hotmail.com>
2020-08-21 23:38:58 -0300

    Página home, Produtos e Categorias

--- src/pages/Home-Main/HomeMain.js
+++ src/pages/Home-Main/HomeMain.js
@@ -8,9 +8,10 @@ export default function HomeMain(){
             <div className="grid-home-main">
                 <section>
                     <h1>
-                        <b>Bebidas geladas</b> a <b>preço</b> de 
-                        mercado na sua casa <b>agora</b>
+                        Zézin Delivery
                     </h1>
+                    <h2><b>Bebidas geladas</b> a <b>preço</b> de 
+                        mercado na sua casa <b>agora</b></h2>
 
                     <div className="localization">
                         <input 
@@ -19,16 +20,73 @@ export default function HomeMain(){
                         />
                     </div>
                 </section>
-
                 <div className="products-category">
-                    <h1>Produtos que você encontra no Zézin</h1>
+                    <h2>Produtos que você encontra no Zézin</h2>
 
                     <div className="products">
                         <div className="group-products">
-                            <div className="products-list"></div>
-                            <div className="products-list"></div>
-                            <div className="products-list"></div>
-                            <div className="products-list"></div>
+                            <div className="products-list">
+                                <div className="products-box">
+                                    <img src="https://ze.delivery/static/img/brahma-lata-350ml.png?auto=compress,format&fit=max&w=undefined&h=268&dpr=2&fm=png" alt=""></img>
+                                </div>
+                                <div className="products-name">
+                                    <span>Cerveja Brahma 250ml</span>
+                                </div>
+                            </div>
+                            <div className="products-list">
+                                <div className="products-box">
+                                    <img src="https://ze.delivery/static/img/budweiser-garrafa-330ml.png?auto=compress,format&fit=max&w=undefined&h=268&dpr=2&fm=png" alt=""></img>
+                                </div>
+                                <div className="products-name">
+                                    <span>Budweiser Garrafa 330ml</span>
+                                </div>
+                            </div>
+                            <div className="products-list">
+                                <div className="products-box">
+                                    <img src="https://ze.delivery/static/img/skol-lata-269ml.png?auto=compress,format&fit=max&w=undefined&h=268&dpr=2&fm=png" alt=""></img>
+                                </div>
+                                <div className="products-name">
+                                    <span>Skol Lata 269ml</span>
+                                </div>
+                            </div>
+                            <div className="products-list">
+                                <div className="products-box">
+                                    <img src="https://ze.delivery/static/img/antarctica-garrafa-300ml.png?auto=compress,format&fit=max&w=undefined&h=268&dpr=2&fm=png" alt=""></img>
+                                </div>
+                                <div className="products-name">
+                                    <span>Antarctica Garrafa 300ml</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="grid-categorys">
+                        <h2>Categorias</h2>
+                        <div className="categorys">
+                            <div className="list-category">
+                                <img src="https://courier-images-frontrelease.imgix.net/category/beer%403x.png?auto=compress,format&fit=max&w=24&h=24&dpr=2&fm=png" alt="Cerveja" />
+                                <strong>Cervejas</strong>
+                            </div>
+                            <div className="list-category">
+                                <img src="https://courier-images-frontrelease.imgix.net/category/distilled%403x.png?auto=compress,format&fit=max&w=24&h=24&dpr=2&fm=png" alt="Destilados" />
+                                <strong>Destilados</strong>
+                            </div>
+                            <div className="list-category">
+                                <img src="https://courier-images-frontrelease.imgix.net/category/wine%403x.png?auto=compress,format&fit=max&w=24&h=24&dpr=2&fm=png" alt="Vinhos" />
+                                <strong>Vinhos</strong>
+                            </div>
+                            <div className="list-category">
+                                <img src="https://courier-images-frontrelease.imgix.net/category/juice%403x.png?auto=compress,format&fit=max&w=24&h=24&dpr=2&fm=png" alt="Sem álcool" />
+                                <strong>Sem álcool</strong>
+                            </div>
+                            <div className="list-category">
+                                <img src="https://courier-images-frontrelease.imgix.net/category/chips%403x.png?auto=compress,format&fit=max&w=24&h=24&dpr=2&fm=png" alt="Petiscos" />
+                                <strong>Petiscos</strong>
+                            </div>
+                            <div className="list-category">
+                                <img src="https://courier-images-frontrelease.imgix.net/category/chips%403x.png?auto=compress,format&fit=max&w=24&h=24&dpr=2&fm=png" alt="Outros" />
+                                <strong>Outros</strong>
+                            </div>
                         </div>
                     </div>
                 </div>
--- src/pages/Home-Main/styled.js
+++ src/pages/Home-Main/styled.js
@@ -27,14 +27,17 @@ export const StyledHomeMain = Styled.div`
                     width: 100%;
                 }
                 font-size: 43px;
-                margin: 1em auto;
+                margin: 0 0 30px;
                 width: 50%;
+                color:#11111b;
+                
+            }
 
+            h2{
                 b{
                     color:#ffcc00;
                 }
             }
-
             div.localization{
                 width:60%;
 
@@ -45,6 +48,7 @@ export const StyledHomeMain = Styled.div`
                     border-radius: 6px;
                     padding: 0 1em;
                     font-size:18px;
+                    margin: 3em auto;
 
                     @media(max-width:600px){
                         width: 100%;
@@ -68,7 +72,7 @@ export const StyledHomeMain = Styled.div`
             justify-content: center;
             flex-direction: column;
 
-            h1{
+            h2{
                 padding:1.4em;
             }
 
@@ -82,15 +86,79 @@ export const StyledHomeMain = Styled.div`
                     overflow: auto;
                     padding: 2em 0;
 
-                    @media(max-width:600px){
+                    @media(max-width:900px){
                         grid-template-columns: none;
                     }
                     div.products-list{
                         height:350px;
-                        width: 25
-                        0px;
+                        width: 250px;
+                        display: grid;
                         margin: 1em;
-                        background: red;
+                        background: #f7f7f794;
+                        box-shadow:3px 3px 7px #cccccc47;
+
+                        div.products-box{
+                            position:relative;
+                            &::before{
+                                content:"";
+                                width:70%;
+                                height:80%;
+                                position:absolute;
+                                margin-left: auto;
+                                margin-right: auto;
+                                left: 0;
+                                top:20%;
+                                right: 0;
+                                text-align: center;
+                                z-index:-1;
+                                border:1px solid #ccc;
+                                border-radius:10%;
+                                background-color:#ffc107;
+                            }
+                            img{
+                                display:block;
+                                width:100%;
+                                z-index:99999;
+                            }
+                        }
+
+                        div.products-name{
+                            display:flex;
+                            justify-content:center;
+                            align-items:center;
+
+                            font-weight:900;
+                        }
+                    }
+                }
+            }
+
+            div.grid-categorys{
+                width:100%;
+                margin:1.5em auto;
+
+                div.categorys{
+                    grid-column-gap:150px;
+                    grid-row-gap:20px;
+                    display:grid;
+                    justify-content: center;
+                    grid-template-columns:repeat(3,200px);
+                    @media(max-width:900px){
+                        grid-template-columns: none;
+                    }
+                    div.list-category{
+                        height:60px;
+                        width:300px;
+                        background:#F5F5F5;
+                        border-radius:8px;
+
+                        display: flex;
+                        align-items: center;
+
+                        img{
+                            width:30px;
+                            margin: 0 9px;
+                        }
                     }
                 }
             }
